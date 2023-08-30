f42158ec3d5c0c6c9e1a624adb5120191e1b2f4f
nikhildubey-23 <ravannick0089@gmail.com>
2023-08-30 07:25:37 +0530

    learn how to make module in python

---
+++ program8.py
@@ -0,0 +1,18 @@
+# How module are made in python?
+# before going forward lets see a new thing 
+#suppose we made a method to calculate the arithmetic solution it use to execute at once only yes there are many method to control that flow but we see the little professional way for that we use main method
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def multi(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+if __name__ == '__main__':
+    add(a,b)
+    sub(a,b)
+    div(a,b)
+    multi(a,b)
+
+# same rest of our import module are made
